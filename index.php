5ebce464f4dfd67e677302d51a81faf90aede135
suddenly-eva <suddenlyeva@gmail.com>
2019-01-15 12:02:55 -0800

    add code for printing a random word from each list file

--- index.php
+++ index.php
@@ -7,7 +7,43 @@
     <body>
 		<p>You should consider getting a
 			<?php
-			// put your code here
+
+        // Load files
+        $f_adjectives = fopen("adjectives", "r");
+        $f_colours = fopen("colours", "r");
+        $f_nouns = fopen("nouns", "r");
+
+        // Create Arrays
+        $adjectives = array();
+        $colours = array();
+        $nouns = array();
+
+        // Load Strings into Arrays
+        while (!feof($f_adjectives)) {
+          array_push($adjectives, fgets($f_adjectives));
+        }
+
+        while (!feof($f_colours)) { 
+          array_push($colours, fgets($f_colours)); 
+        }
+
+        while (!feof($f_nouns)) { 
+          array_push($nouns, fgets($f_nouns)); 
+        } 
+
+        // Pick random colour and print
+        $index = rand(0, sizeof($adjectives) - 1);
+        echo $adjectives[$index];
+        echo " ";
+
+        $index = rand(0, sizeof($colours) - 1);
+        echo $colours[$index];
+        echo " ";
+
+        $index = rand(0, sizeof($nouns) - 1);
+        echo $nouns[$index];
+        echo " ";
+
 			?>
 			for a pet.</p>
     </body>
